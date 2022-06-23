ce49407d189621164c23bf24d59f5df41ac92883
Anointed2022 <105302158+Anointed2022@users.noreply.github.com>
2022-06-23 14:29:56 +0100

    Javascripts code to output name, height and countrt

---
+++ nameoutput.js
@@ -0,0 +1,14 @@
+//types
+var [h1] = ['Hi, my name is'];
+const myheight = '5.8' 
+var [h2] = ['My height is:'];
+var [h3] = ['feets']
+const [country] = ['Nigeria']
+var [h4] = ['I am from ']
+
+// strings
+const [firstName, lastName] = ['Itua A ', 'Ehizojie.']
+const fullname = firstName + "" + lastName;
+console.log(h1, fullname, )
+console.log(h2,myheight, h3)
+console.log(h4,country)
